943b5e453d75662d68582fbadd59d1ca4a6e791b
XueHuaPiaoPiao <nokaki99@gmail.com>
2021-07-07 19:26:39 +0200

    Adding Trade Screen

--- Engine/Factories/QuestFactory.cs
+++ Engine/Factories/QuestFactory.cs
@@ -17,7 +17,7 @@ namespace Engine.Factories
             List<ItemQuantity> itemsToComplete = new List<ItemQuantity>();
             List<ItemQuantity> rewardItems = new List<ItemQuantity>();
 
-            itemsToComplete.Add(new ItemQuantity(9001, 5));
+            itemsToComplete.Add(new ItemQuantity(9001, 3));
             rewardItems.Add(new ItemQuantity(1002, 1));
 
             // Create the quest
