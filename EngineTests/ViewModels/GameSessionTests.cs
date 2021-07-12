208e111345dc8c20d1d9fb506fec420cf256e463
cangnidepigu <finalandeventual@gmail.com>
2021-07-12 23:19:41 +0200

    Creating Unit Tests

---
+++ EngineTests/ViewModels/GameSessionTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Engine.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels.Tests
+{
+    [TestClass]
+    public class GameSessionTests
+    {
+        [TestMethod]
+        public void TestCreateGameSession()
+        {
+            GameSession gameSession = new GameSession();
+
+            Assert.IsNotNull(gameSession.CurrentPlayer);
+            Assert.AreEqual("Town square", gameSession.CurrentLocation.Name);
+        }
+
+        [TestMethod]
+        public void TestPlayerMovesHomeAndIsCompletelyHealedOnKilled()
+        {
+            GameSession gameSession = new GameSession();
+
+            gameSession.CurrentPlayer.TakeDamage(9999);
+
+            Assert.AreEqual("Home", gameSession.CurrentLocation.Name);
+            Assert.AreEqual(gameSession.CurrentPlayer.Level * 10, gameSession.CurrentPlayer.CurrentHitPoints);
+        }
+    }
+}
