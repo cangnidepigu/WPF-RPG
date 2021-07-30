a568ab379dcbab3363fb6e10d1ad4217202a5825
XueHuaPiaoPiao <nokaki99@gmail.com>
2021-07-30 16:14:10 +0200

    Letting the Monster use AttackWithWeapon

--- Engine/Models/Monster.cs
+++ Engine/Models/Monster.cs
@@ -9,19 +9,14 @@ namespace Engine.Models
     public class Monster : LivingEntity
     {
         public string ImageName { get; }
-        public int MinimumDamage { get; }
-        public int MaximumDamage { get; }
         public int RewardExperiencePoints { get; }
 
         public Monster(string name, string imageName,
             int maximuHitPoints, int currentHitPoints,
-            int minimumDamage, int maximumDamage,
             int rewardExperiencePoints, int gold) :
             base(name, maximuHitPoints, currentHitPoints, gold)
         {
             ImageName = $"pack://application:,,,/Engine;component/Images/Monsters/{imageName}";
-            MinimumDamage = minimumDamage;
-            MaximumDamage = maximumDamage;
             RewardExperiencePoints = rewardExperiencePoints;
         }
 
