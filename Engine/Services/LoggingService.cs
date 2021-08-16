b8828c4de4ed4b67435d81f2efff70a14dfffb83
XueHuaPiaoPiao <nokaki99@gmail.com>
2021-08-16 15:32:10 +0200

    Catch and log exceptions

---
+++ Engine/Services/LoggingService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Services
+{
+    public static class LoggingService
+    {
+        private const string LOG_FILE_DIRECTORY = "Logs";
+
+        static LoggingService()
+        {
+            string logDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, LOG_FILE_DIRECTORY);
+
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+        }
+
+        public static void Log(Exception exception, bool isInnerException = false)
+        {
+            using (StreamWriter sw = new StreamWriter(LogFileName(), true))
+            {
+                sw.WriteLine(isInnerException ? "INNER EXCEPTION" : $"EXCEPTION: {DateTime.Now}");
+                sw.WriteLine(new string(isInnerException ? '-' : '=', 40));
+                sw.WriteLine($"{exception.Message}");
+                sw.WriteLine($"{exception.StackTrace}");
+
+                sw.WriteLine();
+            }
+
+            if (exception.InnerException != null)
+            {
+                Log(exception.InnerException, true);
+            }
+        }
+
+        private static string LogFileName()
+        {
+            // This will create a separate log file for each day 
+            // But we are not hoping for many of them
+            return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, LOG_FILE_DIRECTORY,
+                $"SOSCSRPGG_{DateTime.Now:yyyyMMdd}.log");
+        }
+    }
+}
--- WPFUI/App.xaml.cs
+++ WPFUI/App.xaml.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
+using Engine.Services;
 
 namespace WPFUI
 {
@@ -13,5 +14,14 @@ namespace WPFUI
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            string exceptionMessageText =
+                $"An exception occured, you little sussy baka: {e.Exception.Message}\r\n\r\nat: {e.Exception.StackTrace}";
+
+            LoggingService.Log(e.Exception);
+
+            MessageBox.Show(exceptionMessageText, "Unhandled Exception - it brokey", MessageBoxButton.OK);
+        }
     }
 }
